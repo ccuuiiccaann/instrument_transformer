3d67de11d2205cec1848cc237b3a8987a738f946
ccuuiiccaann <cuican@cuican>
2016-05-19 21:15:04 +0800

    去掉无效的包

--- src/instrument_transformer/MainFrame.java
+++ src/instrument_transformer/MainFrame.java
@@ -5,8 +5,6 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
